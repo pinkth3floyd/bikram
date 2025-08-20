20a91540760a413383b14679c21a968e15b17daa
pinkth3floyd <prakash@zeptosky.com>
2025-08-20 20:21:29 +0545

    buttons place

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,26 @@
 'use client'
 
+import { SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
+
+
+
+      <button onClick={() => {
+        window.location.href = '/home/onboarding'
+      }}>
+        Onboarding
+      </button>
+
+<SignOutButton>
+  <button>
+    Logout
+  </button>
+</SignOutButton>
      
 
 
